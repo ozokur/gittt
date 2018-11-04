6d2997166e3bf6cd77f0bd048d3ff9ec1b572544
ozokur <ozokur@me.com>
2018-11-04 12:54:00 +0300

    Initial Commit

---
+++ gittt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittt
+//
+//  Created by Ozan Okur on 4.11.2018.
+//  Copyright © 2018 Ozan Okur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
